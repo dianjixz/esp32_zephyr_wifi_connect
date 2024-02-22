f855818e184e83263e5224cffca662b9f35d1d48
dianjixz <18637716021@163.com>
2024-02-22 18:53:17 +0800

    update

--- Makefile
+++ Makefile
@@ -1,6 +1,19 @@
 
 project_dir=$(shell pwd)
+
+
+HOSTNAME := $(shell hostname)
+
+
+ifeq ($(HOSTNAME), nihao-z690)
+sdk_dir=/home/nihao/work/zephyr/zephyrproject/zephyr
+else ifeq ($(HOSTNAME), hostname2)
+sdk_dir := /path/to/sdk/for/hostname2
+else
 sdk_dir=/home/nihao/work/zephyr/work/zephyrproject/zephyr
+endif
+
+
 
 myboard=esp32
 
--- src/wifi_test.c
+++ src/wifi_test.c
@@ -11,14 +11,34 @@
 typedef enum
 {
     WIFI_NONE = 0,
+    WIFI_CONNECT_ING,
     WIFI_CONNECT
 } wifi_status_t;
 struct wifi_bus_data_t
 {
     wifi_status_t status;
+    struct k_timer wifi_timer;
 } wifi_bus_data;
 
 struct wifi_connect_req_params params;
+
+// void wifi_timer_expiry_function(struct k_timer *timer_id)
+// {
+//     struct net_if *iface = net_if_get_default();
+//     net_mgmt(NET_REQUEST_WIFI_IFACE_STATUS, iface,
+//                 &params, sizeof(struct wifi_connect_req_params));
+
+//     if (wifi_bus_data.status == WIFI_NONE)
+//     {
+
+//         net_mgmt(NET_REQUEST_WIFI_CONNECT, iface,
+//                  &params, sizeof(struct wifi_connect_req_params));
+//     }
+//     else if(wifi_bus_data.status == WIFI_CONNECT)
+//     {
+//         k_timer_stop(&wifi_bus_data.wifi_timer);
+//     }
+// }
 
 static uint32_t scan_result = 1;
 static void wifi_mgmt_event_handler(struct net_mgmt_event_callback *cb,
@@ -86,6 +106,7 @@ static void wifi_mgmt_event_handler(struct net_mgmt_event_callback *cb,
         const struct wifi_status *status = (const struct wifi_status *)cb->info;
         wifi_bus_data.status = WIFI_NONE;
         printk("wifi disconnect !\n");
+        // k_timer_start(&wifi_bus_data.wifi_timer, K_SECONDS(1), K_SECONDS(5);
     }
     break;
     case NET_EVENT_WIFI_TWT:
@@ -122,40 +143,40 @@ static struct net_mgmt_event_callback wifi_mgmt_cb;
 void main(void)
 {
     wifi_bus_data.status = WIFI_NONE;
-    //初始化，可以指定要响应那些事件
+    // 初始化，可以指定要响应那些事件
     net_mgmt_init_event_callback(&wifi_mgmt_cb,
-                    wifi_mgmt_event_handler,
-                    (NET_EVENT_WIFI_SCAN_RESULT |		\
-                    NET_EVENT_WIFI_SCAN_DONE |		\
-                    NET_EVENT_WIFI_CONNECT_RESULT |		\
-                    NET_EVENT_WIFI_DISCONNECT_RESULT));
+                                 wifi_mgmt_event_handler,
+                                 (NET_EVENT_WIFI_SCAN_RESULT |
+                                  NET_EVENT_WIFI_SCAN_DONE |
+                                  NET_EVENT_WIFI_CONNECT_RESULT |
+                                  NET_EVENT_WIFI_DISCONNECT_RESULT));
     net_mgmt_add_event_callback(&wifi_mgmt_cb);
+    // 配置SSID
+    params.ssid = "9";
+    params.ssid_length = strlen(params.ssid);
+    // 配置channel
+    params.channel = WIFI_CHANNEL_ANY;
+    // 配置连接密码
+    params.psk = "1234567890";
+    params.psk_length = strlen(params.psk);
+    // 密码验证方式
+    params.security = WIFI_SECURITY_TYPE_PSK;
+    // 管理帧保护
+    params.mfp = WIFI_MFP_OPTIONAL;
+
+    // k_timer_init(&wifi_bus_data.wifi_timer, wifi_timer_expiry_function, NULL);
+    // k_timer_start(&wifi_bus_data.wifi_timer, K_SECONDS(1), K_SECONDS(5);
 
     struct net_if *iface = net_if_get_default();
     net_mgmt(NET_REQUEST_WIFI_SCAN, iface, NULL, 0);
     k_sleep(K_SECONDS(5));
-    //配置SSID
-    params.ssid = "9";
-    params.ssid_length = strlen(params.ssid);
-
-    //配置channel
-    params.channel = WIFI_CHANNEL_ANY;
-
-    //配置连接密码
-    params.psk = "1234567890";
-    params.psk_length = strlen(params.psk);
-
-    //密码验证方式
-    params.security = WIFI_SECURITY_TYPE_PSK;
-
-    //管理帧保护
-    params.mfp = WIFI_MFP_OPTIONAL;
-
-    //执行连接
+    // int ret = net_mgmt(NET_REQUEST_WIFI_CONNECT, iface,
+    //                     &params, sizeof(struct wifi_connect_req_params));
+    // 执行连接
     while (!wifi_bus_data.status)
     {
         int ret = net_mgmt(NET_REQUEST_WIFI_CONNECT, iface,
-                    &params, sizeof(struct wifi_connect_req_params));
+                           &params, sizeof(struct wifi_connect_req_params));
         printk("will be connect 9\n");
         k_sleep(K_SECONDS(5));
     }
